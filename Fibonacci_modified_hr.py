54dd2eb94a105545f83cb0f63ad72136097554be
Dhruv Srivastava <sridhruv00@gmail.com>
2018-08-23 16:10:16 +0530

    Add files via upload

---
+++ Fibonacci_modified_hr.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    a=[]
+    a.append(t1);
+    a.append(t2);
+    ti=0;
+    for i in range(2,n):
+        ti=a[i-2]+a[i-1]**2;
+        a.append(ti);
+    return (a[n-1]);
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
